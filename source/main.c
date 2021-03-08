504a3ae66c7681a97fdcaa87dc5388562576439c
WidjiWidji <karsten.widjanarko@gmail.com>
2021-03-07 21:12:48 -0800

    completed exercise 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: lab
- *  Partner(s) Name: 
+ *  Partner(s) Name:
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #13  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,12 +12,29 @@
 #include "simAVRHeader.h"
 #endif
 
+//Example: unsigned short input = ADC //short is required to store all 10 bits
+unsigned char tmpB;
+unsigned short input; //for A2D
+
+
+void A2D_init(){
+  ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+  //ADEN: Enables analog-to-digital conversion
+  //ADSC: Starts analog-to-digital conversion
+  //ADATE: Enables auto-triggering, allowing for constant
+  //        analog to digital conversions.
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
+    //Note: you do not need to set the DDRA to enable the Analog to Digital circuitry
+    DDRB = 0xFF; PORTB = 0x00;
 
+    A2D_init();
     /* Insert your solution below */
     while (1) {
-
+      input = ADC;
+      tmpB = (char)input;
+      PORTB = ~tmpB;
     }
     return 1;
 }
